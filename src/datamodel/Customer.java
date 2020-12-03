5034266f9283fe98c953eeb292b8a35792526564
Antonia <s80191@beuth-hochschule.de>
2020-12-03 21:41:28 +0100

    CustomerTest

--- src/datamodel/Customer.java
+++ src/datamodel/Customer.java
@@ -1,53 +1,71 @@
 package datamodel;
 
 public class Customer {
-	
+
 	private final String id;
-	private String firstName;
-	private String lastName;
-	private String contact;
-	
+	private String firstName = "";
+	private String lastName = "";
+	private String contact = "";
+
 	/*
-	 *  Instanzen der Klasse sollen nur durch die DataFactory erzeugt werden können, 
-	 *  also ist der Konstruktor protected
+	 * Instanzen der Klasse sollen nur durch die DataFactory erzeugt werden können,
+	 * also ist der Konstruktor protected
 	 */
-	protected Customer (String id, String name, String contact) {
-		this.id = id;
-		this.contact = contact;
-		this.firstName = "";
-		this.lastName = name;
+	protected Customer(String id, String name, String contact) {
+		// Spezialfall: NullArgumentConstructor, siehe CustomerTest
+		if ((id == null) && (name == null) && (contact == null)) {
+			this.id = null; // id ist null, die anderen Werte sind ""
+			// Normalfall
+		} else {
+			this.id = id;
+			this.contact = contact;
+			this.lastName = name;
+		}
 	}
+
 	/*
-	 * getName() und setName(String name) wurden entfernt,
-	 * die Namenszuweisung und -teilung wird im OutputProcessor organisiert.
-	 */	
-	
+	 * getName() und setName(String name) wurden entfernt, die Namenszuweisung und
+	 * -teilung wird im OutputProcessor organisiert.
+	 */
+
 	public String getId() {
 		return this.id;
 	}
-	
+
 	public String getFirstName() {
 		return this.firstName;
 	}
-	
+
 	public void setFirstName(String firstName) {
-		this.firstName=firstName;
+		if (firstName == null) {
+			this.firstName = "";
+		} else {
+			this.firstName = firstName;
+		}
 	}
-	
+
 	public String getLastName() {
 		return this.lastName;
 	}
-	
+
 	public void setLastName(String lastName) {
-		this.lastName=lastName;
+		if (lastName == null) {
+			this.lastName = "";
+		} else {
+			this.lastName = lastName;
+		}
 	}
 
 	public String getContact() {
 		return this.contact;
 	}
-	
+
 	public void setContact(String contact) {
-		this.contact=contact;
+		if (contact == null) {
+			this.contact = "";
+		} else {
+			this.contact = contact;
+		}
 	}
 
 }
